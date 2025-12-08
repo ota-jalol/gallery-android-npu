5b9c25fab9c4ad7a936e7c8f3f69acf22a5ab51d
otajalol <jaloliddin.otajonov@asbt.com>
2025-12-08 23:14:16 +0500

    Enhance APK build process: add APK listing, renaming, and upload steps; enable ABI splits for optimized builds

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -47,6 +47,15 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+      isUniversalApk = true
+    }
+  }
+
   buildTypes {
     release {
       isMinifyEnabled = false
